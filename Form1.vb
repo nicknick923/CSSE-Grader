30a345c5b1e139184f5e13eab073452989f44e95
jakeira11 <jacob.ira12@gmail.com>
2016-01-19 21:51:04 -0600

    We Gave Up

--- Form1.vb
+++ Form1.vb
@@ -1,12 +1,16 @@
 ﻿Imports System.IO.File
 Public Class Form1
-    Dim openingChar() As Char = {"{", "(", "["}
-    Dim closingChar() As Char = {"}", ")", "]"}
+    Private openingChar() As Char = {"{", "(", "["}
+    Private closingChar() As Char = {"}", ")", "]"}
 
-    Dim ourStack As Stack = New Stack
+    Private ourStack As Stack = New Stack
 
-    Dim inCommentBlock As Boolean = False
-    Dim inCommentLine As Boolean = False
+    Private inCommentBlock As Boolean = False
+    Private inCommentLine As Boolean = False
+
+    Private indentationLevel As Integer = 0
+    Private previousLine As String = ""
+    Private oneLiner As Boolean = False
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         OpenFileDialog1.ShowDialog()
@@ -14,7 +18,6 @@ Public Class Form1
             TextBox1.Text = OpenFileDialog1.FileName
         End If
     End Sub
-
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         OpenFileDialog1.FileName = TextBox1.Text
@@ -30,11 +33,15 @@ Public Class Form1
         For Each line As String In fileStringLines
             line = line.Replace(vbLf, "")
             preCommentCode(line)
+            preIndentationLevelCheck(line)
             lineNumber += 1
             If isLineTooLong(line) Then
                 MsgBox("LINE TOO LONG!!!" & vbCrLf & line)
             End If
             If (Not inCommentBlock) And (Not inCommentLine) Then
+                If indentationErrors(line) Then
+                    MsgBox("INDENTATION ERROR!!!" & vbCrLf & lineNumber & ":" & line)
+                End If
                 For Each charactor As Char In line
                     If isOpeningChar(charactor) Then
                         ourStack.push(charactor)
@@ -51,6 +58,8 @@ Public Class Form1
                     End If
                 Next
             End If
+            postIndentationLevelCheck(line)
+            previousLine = line
         Next
     End Sub
 
@@ -83,6 +92,35 @@ Public Class Form1
                 Return False
         End Select
     End Function
+
+    Private Sub preIndentationLevelCheck(line As String)
+
+        If (Not inCommentBlock) And (Not inCommentLine) Then
+            If line.Replace(" ", "") = "}" Then
+                indentationLevel -= 1
+            End If
+            If previousLine.Contains("for") Or previousLine.Contains("if") Or previousLine.Contains("else") Or previousLine.Contains("while") Then
+                If Not line.Contains("{") Then
+                    indentationLevel += 1
+                    oneLiner = True
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub postIndentationLevelCheck(line As String)
+        If (Not inCommentBlock) And (Not inCommentLine) Then
+            If previousLine.Contains("for") Or previousLine.Contains("if") Or previousLine.Contains("else") Or previousLine.Contains("while") Then
+                If line.Contains("{") Then
+                    indentationLevel += 1
+                End If
+            End If
+            If oneLiner Then
+                indentationLevel -= 1
+                oneLiner = False
+            End If
+        End If
+    End Sub
 
     Private Function oppositeChar(inChar As Char) As Char
         Select Case inChar
@@ -118,4 +156,27 @@ Public Class Form1
             inCommentBlock = False
         End If
     End Sub
+
+    Private Function indentationErrors(line As String) As Boolean
+
+        If line.StartsWith(indentationReturn()) Then
+            If indentationLevel > 0 And line.Replace(indentationReturn(), "").Length > 0 Then
+                If (Not line.Replace(indentationReturn(), "").First() = " ") Then
+                    Return False
+                End If
+            End If
+        End If
+        Return True
+    End Function
+
+    Private Function indentationReturn() As String
+        Dim retString As String = ""
+        Dim x As Integer = 0
+        While x < indentationLevel
+            retString &= "   "
+            x += 1
+        End While
+        Return retString
+    End Function
+
 End Class
---
+++ Module1.vb
@@ -0,0 +1,165 @@
+﻿Module Module1
+    Dim fileLocation As String = My.Application.CommandLineArgs.Item(0)
+    Dim lineList As New List(Of String)
+    Dim curLine As Integer = 0
+    Dim indentLevel As Integer = 0
+    Dim lineTemp As String
+    Dim commentBlock As Boolean
+    Dim extraIndent As String = ""
+    Dim errorString As String = vbNullString
+    Dim magicNumbers As List(Of Integer) = New List(Of Integer)
+    Dim indentationErrors As List(Of Integer) = New List(Of Integer)
+
+    Public Sub onMyLoad(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Public Sub generateFile()
+        errorString = "Magic Number Violations:" & vbCrLf
+        For Each line As Integer In magicNumbers
+            errorString &= " " & line
+        Next
+        errorString &= vbCrLf
+        errorString &= "Indentation Violations:" & vbCrLf
+        For Each line As Integer In indentationErrors
+            errorString &= " " & line
+        Next
+        My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.CurrentDirectory & "/errors.txt", errorString, False)
+    End Sub
+
+
+    Public Sub prepFile()
+        Dim fileString As String = My.Computer.FileSystem.ReadAllText(fileLocation)
+        fileString = fileString.Replace(vbCrLf, vbLf)
+        Dim lineArray() As String = fileString.Split(vbLf)
+        For Each line As String In lineArray
+            lineList.Add(line)
+        Next
+    End Sub
+
+    Public Sub displayFile()
+        Form1.TextBox1.Text = ""
+        Dim textboxstring As String = vbNullString
+        Dim lineNum As Integer = 1
+        For Each line As String In lineList
+            textboxstring &= lineNum & ": " & line & vbCrLf
+            lineNum += 1
+        Next
+        Form1.TextBox1.Text = textboxstring
+    End Sub
+
+    Public Sub readLine()
+        lineTemp = lineList(curLine)
+        If lineTemp.Contains("/*") Or lineTemp.Contains("/*") Then
+            commentBlock = True
+        ElseIf curLine > 0 Then
+
+            If lineList(curLine - 1).Contains("*/") Then
+                commentBlock = False
+            End If
+        End If
+        isIndentOk()
+        If isMagicNumber() Then
+            magicNumbers.Add(curLine + 1)
+        End If
+        curLine += 1
+    End Sub
+
+    Public Function atEnd() As Boolean
+        Return curLine.Equals(lineList.Count)
+    End Function
+
+    Public Sub isLineAboveOpenCurlyBrace()
+        If curLine > 0 Then
+            If lineList(curLine - 1).Contains("{") Then
+                indentLevel += 1
+            End If
+        End If
+    End Sub
+
+    Public Sub doesLineContainClosingCurlyBrace()
+        If lineList(curLine).Contains("}") Then
+            indentLevel -= 1
+        End If
+    End Sub
+
+    Private Sub isIndentOk()
+        doesLineContainClosingCurlyBrace()
+        isLineAboveOpenCurlyBrace()
+        isOneLineIndent()
+        Dim firstNum As Integer = 0
+        Dim secondNum As Integer = (3 * indentLevel)
+        If commentBlock Then
+            If Not (lineList(curLine).Length = 0) Then
+                Dim expectedIndent As String = indentGenerator()
+                Dim givenIndent As String = lineList(curLine).Substring(0, secondNum)
+                If lineList(curLine).StartsWith(expectedIndent & " ") And Not lineList(curLine).StartsWith(expectedIndent & "/") Then
+                    If lineList(curLine).StartsWith(expectedIndent & "  ") Then
+                        indentationErrors.Add(curLine + 1)
+                    End If
+                End If
+            End If
+        Else
+            If Not (lineList(curLine).Length = 0) Then
+                Dim expectedIndent As String = indentGenerator()
+                Dim givenIndent As String = lineList(curLine).Substring(0, secondNum)
+                If lineList(curLine).StartsWith(expectedIndent & extraIndent) And Not lineList(curLine).StartsWith(expectedIndent & extraIndent & " ") Then
+                Else
+                    If Not lineTemp.Replace(" ", "").StartsWith("+") Then
+                        If Not lineList(curLine - 1).EndsWith(",") Then
+                            indentationErrors.Add(curLine + 1)
+                        End If
+                    End If
+                End If
+            End If
+        End If
+        extraIndent = ""
+    End Sub
+
+    Private Function indentGenerator() As String
+        Dim retString As String = ""
+        Dim i As Integer = 0
+        While (i < indentLevel)
+            retString &= "   "
+            i += 1
+        End While
+        Return retString
+    End Function
+
+    Private Sub isOneLineIndent()
+        Dim myCurLine As Integer = curLine
+        If curLine > 0 And myCurLine > 0 Then
+            While lineList(myCurLine - 1).Contains("if") Or lineList(myCurLine - 1).Contains("else") Or lineList(myCurLine - 1).Contains("for") Or lineList(myCurLine - 1).Contains("while")
+                If Not lineTemp.Contains("{") Then
+                    extraIndent &= "   "
+                End If
+                myCurLine -= 1
+            End While
+        End If
+    End Sub
+
+    Private Function isMagicNumber() As Boolean
+        Dim containsNum As Boolean = False
+        Dim prevChar As Char = vbNullChar
+        For Each myChar As Char In lineTemp
+            If Char.IsNumber(myChar) Then
+                Select Case myChar
+                    Case "0"
+                        If Char.IsNumber(prevChar) Then
+                            containsNum = True
+                        End If
+                    Case "1"
+                        If Char.IsNumber(prevChar) Then
+                            containsNum = True
+                        End If
+                    Case Else
+                        containsNum = True
+                End Select
+            End If
+            prevChar = myChar
+        Next
+        Return containsNum And Not lineTemp.Contains("final")
+    End Function
+
+
+End Module
