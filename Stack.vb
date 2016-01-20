7b194bce92c51e076627403872f9df5cdd98c8e1
jakeira11 <jacob.ira12@gmail.com>
2016-01-19 18:15:14 -0600

    Worked on creating a stack class.  WOrked on it at least

---
+++ Stack.vb
@@ -0,0 +1,16 @@
+﻿Public Class Stack
+
+    Private top As Node
+
+    Private Boolean isEmpty()
+        top == null
+    End Boolean
+    Private Sub push(inString As String)
+        top = New Node(inString, top)
+    End Sub
+
+    Private Function pop() As String
+        If (isEmpty()) Then
+            Return ""
+    End Function
+End Class
